bf859f435b5fe6755dbe5f52476046022a787eb6
sensuikan1973 <sensuikan1973@gmail.com>
2026-06-03 15:06:36 +0900

    add jule setup script

---
+++ .jules/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euxo pipefail
+
+# https://docs.flutter.dev/get-started/install/linux/desktop#install-the-flutter-sdk
+# https://docs.flutter.dev/install/archive#main-channel
+
+git clone https://github.com/flutter/flutter
+sudo mv flutter /usr/local/
+export PATH=$PATH:/usr/local/flutter/bin
+export PATH="$PATH:/usr/local/flutter/bin" >> ~/.bash_profile
+
+flutter --version
+dart --version
+
+./scripts/setup_flutter.sh
