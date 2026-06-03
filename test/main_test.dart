40eaf0a04092394d4659612e175b7f94a56bf48a
sensuikan1973 <sensuikan1973@gmail.com>
2026-06-03 12:29:10 +0900

    a

--- test/main_test.dart
+++ test/main_test.dart
@@ -28,7 +28,8 @@ void main() {
         libName = 'libedax-x64.dll';
       } else {
         throw UnsupportedError(
-            'Unsupported platform: ${Platform.operatingSystem}');
+          'Unsupported platform: ${Platform.operatingSystem}',
+        );
       }
       final dllPath = p.join(baseDir, 'resources', 'dll', libName);
       final evalPath = p.join(baseDir, 'resources', 'data', 'eval.dat');
@@ -52,10 +53,7 @@ void main() {
 
       server = EdaxMcpServer(
         serverChannel,
-        implementation: Implementation(
-          name: 'test_server',
-          version: '0.0.1',
-        ),
+        implementation: Implementation(name: 'test_server', version: '0.0.1'),
         libEdax: libEdax,
       );
     });
@@ -69,42 +67,52 @@ void main() {
     test('get_moves tool returns current moves', () async {
       // 1. Initialize
       final initId = 1;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': initId,
-        'method': 'initialize',
-        'params': <String, dynamic>{
-          'protocolVersion': '2024-11-05',
-          'capabilities': <String, dynamic>{},
-          'clientInfo': <String, String>{'name': 'test-client', 'version': '1.0.0'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': initId,
+          'method': 'initialize',
+          'params': <String, dynamic>{
+            'protocolVersion': '2024-11-05',
+            'capabilities': <String, dynamic>{},
+            'clientInfo': <String, String>{
+              'name': 'test-client',
+              'version': '1.0.0',
+            },
+          },
+        }),
+      );
 
       // Wait for initialize response
       await serverResponses.firstWhere((m) => m['id'] == initId);
 
       // 2. Initialized notification
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'method': 'notifications/initialized'
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'method': 'notifications/initialized',
+        }),
+      );
 
       await server.initialized;
 
       // 3. Call get_moves
       final callId = 2;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': callId,
-        'method': 'tools/call',
-        'params': <String, dynamic>{
-          'name': 'get_moves',
-          'arguments': <String, dynamic>{}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': callId,
+          'method': 'tools/call',
+          'params': <String, dynamic>{
+            'name': 'get_moves',
+            'arguments': <String, dynamic>{},
+          },
+        }),
+      );
 
-      final response =
-          await serverResponses.firstWhere((m) => m['id'] == callId);
+      final response = await serverResponses.firstWhere(
+        (m) => m['id'] == callId,
+      );
 
       expect(response['result']['content'][0]['text'], isA<String>());
     });
@@ -112,40 +120,50 @@ void main() {
     test('edax_hint tool returns hints', () async {
       // 1. Initialize
       final initId = 1;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': initId,
-        'method': 'initialize',
-        'params': <String, dynamic>{
-          'protocolVersion': '2024-11-05',
-          'capabilities': <String, dynamic>{},
-          'clientInfo': <String, String>{'name': 'test-client', 'version': '1.0.0'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': initId,
+          'method': 'initialize',
+          'params': <String, dynamic>{
+            'protocolVersion': '2024-11-05',
+            'capabilities': <String, dynamic>{},
+            'clientInfo': <String, String>{
+              'name': 'test-client',
+              'version': '1.0.0',
+            },
+          },
+        }),
+      );
 
       await serverResponses.firstWhere((m) => m['id'] == initId);
 
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'method': 'notifications/initialized'
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'method': 'notifications/initialized',
+        }),
+      );
 
       await server.initialized;
 
       // 2. Call edax_hint
       final callId = 2;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': callId,
-        'method': 'tools/call',
-        'params': <String, dynamic>{
-          'name': 'edax_hint',
-          'arguments': <String, dynamic>{'n': 2}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': callId,
+          'method': 'tools/call',
+          'params': <String, dynamic>{
+            'name': 'edax_hint',
+            'arguments': <String, dynamic>{'n': 2},
+          },
+        }),
+      );
 
-      final response =
-          await serverResponses.firstWhere((m) => m['id'] == callId);
+      final response = await serverResponses.firstWhere(
+        (m) => m['id'] == callId,
+      );
 
       final text = response['result']['content'][0]['text'] as String;
       expect(text, contains('Hint 1:'));
@@ -155,169 +173,226 @@ void main() {
     test('play_move tool plays a move', () async {
       // 1. Initialize
       final initId = 1;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': initId,
-        'method': 'initialize',
-        'params': <String, dynamic>{
-          'protocolVersion': '2024-11-05',
-          'capabilities': <String, dynamic>{},
-          'clientInfo': <String, String>{'name': 'test-client', 'version': '1.0.0'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': initId,
+          'method': 'initialize',
+          'params': <String, dynamic>{
+            'protocolVersion': '2024-11-05',
+            'capabilities': <String, dynamic>{},
+            'clientInfo': <String, String>{
+              'name': 'test-client',
+              'version': '1.0.0',
+            },
+          },
+        }),
+      );
 
       await serverResponses.firstWhere((m) => m['id'] == initId);
 
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'method': 'notifications/initialized'
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'method': 'notifications/initialized',
+        }),
+      );
 
       await server.initialized;
 
       // 2. Play move
       final callId = 2;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': callId,
-        'method': 'tools/call',
-        'params': <String, dynamic>{
-          'name': 'play_move',
-          'arguments': <String, dynamic>{'move': 'f5'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': callId,
+          'method': 'tools/call',
+          'params': <String, dynamic>{
+            'name': 'play_move',
+            'arguments': <String, dynamic>{'move': 'f5'},
+          },
+        }),
+      );
 
-      final response =
-          await serverResponses.firstWhere((m) => m['id'] == callId);
-      expect(response['result']['content'][0]['text'], contains('Played move: f5'));
+      final response = await serverResponses.firstWhere(
+        (m) => m['id'] == callId,
+      );
+      expect(
+        response['result']['content'][0]['text'],
+        contains('Played move: f5'),
+      );
 
       // 3. Verify move is played
       final callId2 = 3;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': callId2,
-        'method': 'tools/call',
-        'params': <String, dynamic>{
-          'name': 'get_moves',
-          'arguments': <String, dynamic>{}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': callId2,
+          'method': 'tools/call',
+          'params': <String, dynamic>{
+            'name': 'get_moves',
+            'arguments': <String, dynamic>{},
+          },
+        }),
+      );
 
-      final response2 =
-          await serverResponses.firstWhere((m) => m['id'] == callId2);
-      expect(response2['result']['content'][0]['text'].toLowerCase(),
-          contains('f5'));
+      final response2 = await serverResponses.firstWhere(
+        (m) => m['id'] == callId2,
+      );
+      expect(
+        response2['result']['content'][0]['text'].toLowerCase(),
+        contains('f5'),
+      );
     });
 
     test('get_board tool returns board state', () async {
       // 1. Initialize
       final initId = 1;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': initId,
-        'method': 'initialize',
-        'params': <String, dynamic>{
-          'protocolVersion': '2024-11-05',
-          'capabilities': <String, dynamic>{},
-          'clientInfo': <String, String>{'name': 'test-client', 'version': '1.0.0'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': initId,
+          'method': 'initialize',
+          'params': <String, dynamic>{
+            'protocolVersion': '2024-11-05',
+            'capabilities': <String, dynamic>{},
+            'clientInfo': <String, String>{
+              'name': 'test-client',
+              'version': '1.0.0',
+            },
+          },
+        }),
+      );
       await serverResponses.firstWhere((m) => m['id'] == initId);
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'method': 'notifications/initialized'
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'method': 'notifications/initialized',
+        }),
+      );
       await server.initialized;
 
       // 2. Call get_board
       final callId = 2;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': callId,
-        'method': 'tools/call',
-        'params': <String, dynamic>{
-          'name': 'get_board',
-          'arguments': <String, dynamic>{}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': callId,
+          'method': 'tools/call',
+          'params': <String, dynamic>{
+            'name': 'get_board',
+            'arguments': <String, dynamic>{},
+          },
+        }),
+      );
 
-      final response =
-          await serverResponses.firstWhere((m) => m['id'] == callId);
-      expect(response['result']['content'][0]['text'], contains('A B C D E F G H'));
-      expect(response['result']['structuredContent']['player_bitboard'], isA<String>());
+      final response = await serverResponses.firstWhere(
+        (m) => m['id'] == callId,
+      );
+      expect(
+        response['result']['content'][0]['text'],
+        contains('A B C D E F G H'),
+      );
+      expect(
+        response['result']['structuredContent']['player_bitboard'],
+        isA<String>(),
+      );
     });
 
     test('get_mobility_count returns count', () async {
       // 1. Initialize
       final initId = 1;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': initId,
-        'method': 'initialize',
-        'params': <String, dynamic>{
-          'protocolVersion': '2024-11-05',
-          'capabilities': <String, dynamic>{},
-          'clientInfo': <String, String>{'name': 'test-client', 'version': '1.0.0'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': initId,
+          'method': 'initialize',
+          'params': <String, dynamic>{
+            'protocolVersion': '2024-11-05',
+            'capabilities': <String, dynamic>{},
+            'clientInfo': <String, String>{
+              'name': 'test-client',
+              'version': '1.0.0',
+            },
+          },
+        }),
+      );
       await serverResponses.firstWhere((m) => m['id'] == initId);
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'method': 'notifications/initialized'
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'method': 'notifications/initialized',
+        }),
+      );
       await server.initialized;
 
       // 2. Call get_mobility_count
       final callId = 2;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': callId,
-        'method': 'tools/call',
-        'params': <String, dynamic>{
-          'name': 'get_mobility_count',
-          'arguments': <String, dynamic>{'color': 'black'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': callId,
+          'method': 'tools/call',
+          'params': <String, dynamic>{
+            'name': 'get_mobility_count',
+            'arguments': <String, dynamic>{'color': 'black'},
+          },
+        }),
+      );
 
-      final response =
-          await serverResponses.firstWhere((m) => m['id'] == callId);
-      expect(response['result']['content'][0]['text'], contains('Mobility count for black:'));
+      final response = await serverResponses.firstWhere(
+        (m) => m['id'] == callId,
+      );
+      expect(
+        response['result']['content'][0]['text'],
+        contains('Mobility count for black:'),
+      );
     });
 
     test('play_print returns summary', () async {
       // 1. Initialize
       final initId = 1;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': initId,
-        'method': 'initialize',
-        'params': <String, dynamic>{
-          'protocolVersion': '2024-11-05',
-          'capabilities': <String, dynamic>{},
-          'clientInfo': <String, String>{'name': 'test-client', 'version': '1.0.0'}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': initId,
+          'method': 'initialize',
+          'params': <String, dynamic>{
+            'protocolVersion': '2024-11-05',
+            'capabilities': <String, dynamic>{},
+            'clientInfo': <String, String>{
+              'name': 'test-client',
+              'version': '1.0.0',
+            },
+          },
+        }),
+      );
       await serverResponses.firstWhere((m) => m['id'] == initId);
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'method': 'notifications/initialized'
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'method': 'notifications/initialized',
+        }),
+      );
       await server.initialized;
 
       // 2. Call play_print
       final callId = 2;
-      clientToServer.add(jsonEncode(<String, dynamic>{
-        'jsonrpc': '2.0',
-        'id': callId,
-        'method': 'tools/call',
-        'params': <String, dynamic>{
-          'name': 'play_print',
-          'arguments': <String, dynamic>{}
-        }
-      }));
+      clientToServer.add(
+        jsonEncode(<String, dynamic>{
+          'jsonrpc': '2.0',
+          'id': callId,
+          'method': 'tools/call',
+          'params': <String, dynamic>{
+            'name': 'play_print',
+            'arguments': <String, dynamic>{},
+          },
+        }),
+      );
 
-      final response =
-          await serverResponses.firstWhere((m) => m['id'] == callId);
+      final response = await serverResponses.firstWhere(
+        (m) => m['id'] == callId,
+      );
       final text = response['result']['content'][0]['text'] as String;
       expect(text, contains('Discs:'));
       expect(text, contains('Mobility:'));
